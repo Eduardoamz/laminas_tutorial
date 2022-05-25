cebc5024b96ad5b53f3c41cd0189d9b60f7a1989
eduardoamz <eduardo@amzdig.com>
2022-05-25 10:48:24 -0300

    final commit of laminas project
    bom dia

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,6 +6,8 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Paginator',
+    'Laminas\Navigation',
     'Laminas\Mvc\I18n',
     'Laminas\I18n',
     'Laminas\Hydrator',
